d7c527549301dd9395575ce392ea21d0e8d0a994
Glenn T <glenntm1@live.com>
2024-09-03 22:55:46 -0500

    added login and logout feature

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
-from flask_login import UserMixin
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from secret import database_username, database_secret, databse_name, databse_password
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, Form
 from wtforms.validators import InputRequired,Length, ValidationError,DataRequired, Email
-from models import User
+from models import User, db, connect_db
 from flask_bcrypt import Bcrypt
 import psycopg2
 from psycopg2 import sql
-
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{database_username}:{databse_password}@localhost:5432/{databse_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = f'{database_secret}'
-db = SQLAlchemy(app)
+
+connect_db(app)
+
+
+#connect_db(app)
 
 db_host = 'localhost'
 db_port = '5432'  # Default PostgreSQL port
@@ -26,8 +29,15 @@ db_user = database_username
 db_password = databse_password
 bcrypt = Bcrypt(app)
 
-
 migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class RegistrationForm(FlaskForm):
@@ -85,6 +95,12 @@ def reviews_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('user_dashboard'))
     return render_template('login.html', form=form)
 
 # Route for registration
@@ -111,6 +127,16 @@ def register():
             print(f"Error occurred: {e}")
 
     return render_template('register.html', form=form)
+
+@app.route('/user_dashboard', methods=['GET', 'POST'])
+@login_required
+def user_dashboard():
+    return render_template('user_dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy 
+from flask_login import UserMixin
 
 
 db = SQLAlchemy()
 
 def connect_db(app):
+    """Connect this database to the provided Flask app."""
     db.app = app
     db.init_app(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(80), nullable=False)
